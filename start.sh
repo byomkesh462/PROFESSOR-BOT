f964c068b3c614eed8b79fb6173bcb5ef32c18e0
Byomkesh <88849299+byomkesh462@users.noreply.github.com>
2025-07-22 03:19:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
 
-# Update & upgrade system
-apt update && apt upgrade -y
-
-apt install git -y           
-pip install -U pip    
-
-# Clone the repo 
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/MrMKN/PROFESSOR-BOT /ProfessorBot
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /ProfessorBot
-fi
-
-# Upgrade pip and install requirements
-cd /ProfessorBot
-pip install -U -r requirements.txt --force-reinstall
 
 # Start bot
 echo "Starting Bot....✨"
